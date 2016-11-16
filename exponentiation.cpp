a881721ac8f330c6339ca6eafc0d854345d8ea38
zzy <1253936411@qq.com>
2016-11-16 21:29:59 +0800

    add new problem

---
+++ exponentiation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*
+in order to finish exponentiation,
+we need to implement the big number multiplication
+the big number add function
+*/
+
+string bigAdd(string a,string b)
+{
+string sum="";
+
+return  sum;
+}
+
+int main()
+{
+	long long a,b,sum;
+	while(cin>>a>>b){
+	sum=a+b;
+	cout<<sum<<endl;
+}
+	return 0;
+}
